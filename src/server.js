e8218ef7b4c63d01616542776a01705bb35253c0
falfisme <faqihuddinalfarisi@gmail.com>
2021-05-08 21:45:53 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+console.log('Kita akan membuat RESTful API dengan nodemon dan hapi');
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+        cors: {
+            origin:['*'],
+        }
+    }
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
